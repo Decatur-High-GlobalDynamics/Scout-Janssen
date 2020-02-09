995571375d3b7c05bc233c9b4e08d0cf401ed09d
xHayden <93haydcarp@csdecatur.net>
2020-02-09 11:42:29 -0500

    honestly not sure what ive done

--- scoutjanssen/scoutjanssen/urls.py
+++ scoutjanssen/scoutjanssen/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from scoutingtool import views as scoutview
 
 urlpatterns = [
     path('scout/', include('scoutingtool.urls')),
     path('admin/', admin.site.urls),
+    path('', scoutview.submitReport, name='submitReport'),
 ]
