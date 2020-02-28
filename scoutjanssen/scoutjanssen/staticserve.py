5f5b010021d4694e5c315bf2927a6ce5c4178792
xHayden <haydencarp@outlook.com>
2020-02-28 17:13:50 -0500

    change in static server

--- scoutjanssen/scoutjanssen/staticserve.py
+++
@@ -1,9 +0,0 @@
-from django.views.static import serve as staticserve
-import webapps.settings as settings
-from django.urls import path
-
-def serve(request, what):
-   response = staticserve(request, what,
-              document_root=settings.MEDIA_ROOT)
-   response['Cache-Control'] = 'no-cache'
-   return response
