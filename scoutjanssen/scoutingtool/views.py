28511a7e388562f93c8ec9f56651f4d670f2ccd6
xHayden <93haydcarp@csdecatur.net>
2020-03-02 23:02:48 -0500

    yes

--- scoutjanssen/scoutingtool/views.py
+++ scoutjanssen/scoutingtool/views.py
@@ -131,7 +131,7 @@ def matchPage(request, number):
     team4 = matchInfo[0].team4_id
     team5 = matchInfo[0].team5_id
     team6 = matchInfo[0].team6_id
-    report1 = Report.objects.filter(team_id = "1414").filter(match_id = "115")[0]
+    report1 = Report.objects.filter(team_id = "1414").filter(match_id = "115")
 
 
     return render(request, 'scoutingtool/matchPage.html', {'matchInfo' : matchInfo, 'report1' : report1})
