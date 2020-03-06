7b324e1064d2d608bae384c2802c57b29ada7b5b
xHayden <haydencarp@outlook.com>
2020-03-06 08:55:37 -0500

    dalton

--- scoutjanssen/scoutingtool/admin.py
+++ scoutjanssen/scoutingtool/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Report)
+admin.site.register(Match)
+admin.site.register(Team)
+admin.site.register(CurrentScouting)
+admin.site.register(Schedule)
+admin.site.register(Event)
