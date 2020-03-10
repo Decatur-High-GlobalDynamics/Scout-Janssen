5f107a53c8f04e3c4a82ee5a5bafa9f7242fc07d
xHayden <haydencarp@outlook.com>
2020-03-10 19:09:19 -0400

    duplicates begone

--- scoutjanssen/scoutingtool/urls.py
+++ scoutjanssen/scoutingtool/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
 #from . import views
+urlpatterns = [
+    
+]
+
 '''
 urlpatterns = [
     path('', views.submitReport, name="submitReport"),
--- scoutjanssen/scoutjanssen/urls.py
+++ scoutjanssen/scoutjanssen/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 #from scoutingtool import views as scoutview
+urlpatterns = [
+    
+]
 '''
 urlpatterns = [
     path('scout/', include('scoutingtool.urls')),
