21ddf2960e4dbb9847d3f3524b07452427af2214
xHayden <haydencarp@outlook.com>
2020-12-06 13:09:39 -0500

    oopsies v2

--- scoutjanssen/scoutjanssen/settings.py
+++ scoutjanssen/scoutjanssen/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.auth',
-    'django.contrib.messages',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
