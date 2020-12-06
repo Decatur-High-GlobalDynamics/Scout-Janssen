bc03a9cf9e055bc1583cf74c1810b23c4fffe54f
xHayden <haydencarp@outlook.com>
2020-12-06 00:25:23 -0500

    Update views.py

--- scoutjanssen/scoutingtool/views.py
+++ scoutjanssen/scoutingtool/views.py
@@ -138,7 +138,7 @@ def matchPage(request, number):
     team4 = matchInfo[0].team4_id
     team5 = matchInfo[0].team5_id
     team6 = matchInfo[0].team6_id
-    report1 = Report.objects.filter(team_id = "5734").filter(match_id = "265")
+    report1 = Report.objects.filter(team_id = "5734").filter(match_id = "265")[0]
     
 
     return render(request, 'scoutingtool/matchPage.html', {'matchInfo' : matchInfo, 'report1': report1})
